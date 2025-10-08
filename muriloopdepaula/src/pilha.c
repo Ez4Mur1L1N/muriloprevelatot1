1aaf7c8243c38e2b91f33bcc0a9a1478f175d236
Murilo Oliveira Prevelato de Paula <muriloprevelato17@gmail.com>
2025-10-08 12:20:11 -0300

    Correção do TAD de pilha e implementação do código (.c).

---
+++ muriloopdepaula/src/pilha.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+typedef struct nos{
+    void *elemento; // Ponteiro genérico para receber qualquer elemento.
+    struct nos *prox;
+} NOS;
+
+typedef NOS *NO;
+
+typedef struct{
+    NO topo;
+} PILHAP;
+
+/*
+Estrutura da pilha definida: possui nós, que contém um elemento e um ponteiro apontando pro
+próximo nó. Além disso, é definida pelo topo.
+*/
+
+PILHA criaPilha(){
+    PILHAP *pi = (PILHAP*) malloc(sizeof(PILHAP));
+
+    if(pi == NULL){
+        printf("Erro ao alocar memoria! Programa encerrado.");
+        exit(1);
+    }
+    pi->topo = NULL;
+    return pi;
+}
+
+int pilhaVazia(PILHA p){
+    PILHAP* pi = (PILHAP*) p;
+
+    if(pi->topo == NULL){
+        return 1;
+    } else{
+        return 0;
+    }
+}
+
+void push(PILHA p, void *elemento){
+    PILHAP* pi = (PILHAP*) p;
+
+    NO novo = (NO) malloc(sizeof(NOS));
+    if(novo == NULL){
+        printf("Erro ao alocar memoria! Programa Encerrado.");
+        exit(1);
+    }
+    novo->elemento = elemento;
+    novo->prox = pi->topo;
+    pi->topo = novo;
+}
+
+void *pop(PILHA p){
+    PILHAP* pi = (PILHAP*) p;
+    
+    if(pilhaVazia(p)){ //Pilha vazia
+        return NULL;
+    }
+
+
+    NO retira = pi->topo;
+    void *elementoRetirado = pi->topo->elemento;
+
+    pi->topo = pi->topo->prox;
+    free(retira);
+    return elementoRetirado;
+}
+
+void *getTopo(PILHA p){
+    PILHAP* pi = (PILHAP*) p;
+
+    if(pilhaVazia((p))){
+        return NULL;
+    } else{
+        return pi->topo->elemento;
+    }
+}
+
+void destroiPilha(PILHA p){
+    PILHAP* pi = (PILHAP*) p;
+
+    while(!pilhaVazia(p)){
+        pop(p);
+    }
+    // Enquanto NÃO estiver vazia, percorre ela dando pop e depois libera a pilha.
+
+    free(pi);
+}
--- muriloopdepaula/src/pilha.h
+++ muriloopdepaula/src/pilha.h
@@ -1,10 +1,6 @@
 #ifndef PILHA_H
 #define PILHA_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 /*
 No contexto do projeto, a Pilha é a estrutura de dados
 utilizada para implementar os Carregadores de Formas dos Disparadores.
@@ -15,7 +11,6 @@ O TAD Carregador utilizará esta Pilha como sua estrutura de dados principal.
 */
 
 typedef void* PILHA;
-typedef void* Forma;
 
 /**
  * @brief Cria uma nova pilha vazia.
@@ -23,48 +18,11 @@ typedef void* Forma;
  * @return Retorna um ponteiro para pilha criada.
  *
  * @details Aloca dinamicamente memória para a pilha, atribuindo ponteiro 
- * para o fim da Fila.
+ * para o topo da pilha .
  *
  * @warning Caso a alocacao de memoria dê errado, encerra o programa.
  */
 PILHA criarPilha();
-
-/**
- * @brief Insere um elemento no topo da pilha (empilha).
- * 
- * @param PILHA ponteiro que indica em qual pilha será inserido o elemento.
- *
- * @param Forma Indica qual forma geometrica será inserida.
- *
- * @details Função criada para a inserção, dinâmica, de um elemento.
- * Essa função cria um novo nó no final da pilha e insere a forma lá.
- * O novo nó passa a apontar para o topo 'antigo'.
- *
- * @warning Caso a alocacao de memoria dê errado, encerra o programa.
- */
-void push(PILHA p, Forma g);
-
-/**
- * @brief Remove um elemento do topo da pilha (desempilha).
- *
- * @param PILHA Ponteiro que indica em qual pilha o elemento será retirado.
- * 
- * @details Remove o elemento do topo da pilha, liberando a memória e reajustando os ponteiros.
- * 
- * @warning Fecha o programa caso a fila esteja vazia.
- */
-void pop(PILHA p);
-
-/**
- * @brief 'Pega' o primeiro elemento da pilha sem tirá-lo.
- *
- * @param PILHA Ponteiro que indica em qual pilha o elemento será obtido.
- * 
- * @return Retonar um ponteiro para o elemento do topo da pilha.
- *
- * @warning Retorna um ponteiro NULL caso pilha vazia.
- */
-Form getTopo(PILHA p);
 
 /**
  * @brief Libera memória da pilha e de seus nós (elementos).
@@ -74,6 +32,54 @@ Form getTopo(PILHA p);
  * @details Percorre cada nó e vai desalocando memória (free).
  * Por fim, reajusta os ponteiros inicio e final para NULL.
  */
-void apagaPilha(PILHA p);
+void destroiPilha(PILHA p);
+
+/**
+ * @brief Função que insere um elemento no topo da pilha (empilha).
+ * 
+ * @param PILHA Ponteiro que indica em qual pilha será inserido o elemento.
+ *
+ * @param elemento Ponteiro genérico que ndica qual forma geometrica será inserida.
+ *
+ * @details Função criada para a inserção, dinâmica, de um elemento.
+ * Essa função cria um novo nó no final da pilha e insere a forma lá.
+ * O novo nó passa a apontar para o topo 'antigo'.
+ *
+ * @warning Caso a alocacao de memoria dê errado, encerra o programa.
+ */
+void push(PILHA p, void *elemento);
+
+/**
+ * @brief Função que remove um elemento do topo da pilha (desempilha).
+ *
+ * @param PILHA Ponteiro que indica em qual pilha o elemento será retirado.
+ * 
+ * @details Remove o elemento do topo da pilha, liberando a memória e reajustando os ponteiros.
+ *
+ * @return Um ponteiro para a forma do topo retirado.
+ * 
+ * @warning Retorna NULL caso a fila esteja vazia.
+ */
+void *pop(PILHA p);
+
+/**
+ * @brief Função que 'pega' o primeiro elemento da pilha sem tirá-lo.
+ *
+ * @param PILHA Ponteiro que indica em qual pilha o elemento será obtido.
+ * 
+ * @return Retonar um ponteiro para o elemento do topo da pilha.
+ *
+ * @warning Retorna um ponteiro NULL caso pilha vazia.
+ */
+void *getTopo(PILHA p);
+
+/**
+ * @brief Função que verifica se a pilha está vazia. 
+ * 
+ * @param PILHA
+ * 
+ * @return 1 - para vazia 0 - para algum conteúdo dentro 
+ */
+int pilhaVazia(PILHA p);
 
 #endif
